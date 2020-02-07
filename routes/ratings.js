b21f0471756959360878f103d4678a2844a993b9
Stiward_Solano <13769238+StiwardSolano@users.noreply.github.com>
2020-02-07 02:27:01 -0500

    Update ratings.js

--- routes/ratings.js
+++ routes/ratings.js
@@ -67,7 +67,8 @@ var getWorstRated = [
     },{ $match: { "message": {$in: [/^mal/, /^Ma/,/^Pes/,/^Fal/, /^Horr/
         , /^Tard/, /^Baja/, /^Nada/, /^No e/, /^injusta/
         , /^Mas din/, /^más din/, /^Más din/, /^Debería/, /^.*\b(Mas din|más din|Más din)\b.*$/
-        , /^más tra/, /^Más tra/ , /^ agrega /, /^muy comp/, /^.*\b(Muy comp|muy comp|Muy Comp)\b.*$/
+        , /^más tra/, /^Más tra/ , /^.*\b(más tra|Más tra|agrega más)\b.*$/
+        , /^muy comp/, /^.*\b(Muy comp|muy comp|Muy Comp)\b.*$/
         , /^No cali/, /^no just/, /^injusto/, /^.*\b(No tiene|no buena manera|no explicar)\b.*$/]} } 
     },
     {$group: 
